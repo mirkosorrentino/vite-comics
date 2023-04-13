d0786f1f689a1998fc8f44a865b101aaa6556726
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-04-13 16:17:15 +0200

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: "AppHeader",
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+  <header>
+    <div class="logo"></div>
+    <div class="nav-bar"></div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
