a44a084a4a590c0ce885261b34a1b8bd2a7e1e33
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-04-14 16:49:15 +0200

    Add: jumbotron

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,22 @@ import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppBanner from './components/AppBanner.vue';
 import AppFooter from './components/AppFooter.vue';
+import AppJumbotron from './components/AppJumbotron.vue';
 
 export default {
   components: {
     AppHeader,
     AppMain,
     AppBanner,
-    AppFooter
+    AppFooter,
+    AppJumbotron
   }
 }
 </script>
 
 <template>
     <AppHeader/>
+    <AppJumbotron/>
     <AppMain/>
     <AppBanner/>
     <AppFooter/>
---
+++ src/components/AppJumbotron.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: "AppJumbotron",
+}
+</script>
+
+<template>
+    <div class="jumbotron">
+        <img src="../assets/img/jumbotron.jpg" alt="DC jumbotron">
+    </div>
+</template>
+
+<style lang="scss" scoped>
+// @use "../style/partials/variables" as *;
+
+.jumbotron{
+    height: 300px;
+    
+    img {
+        width: 100%;
+    }
+}
+</style>
