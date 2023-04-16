dbca0395a842b3c8ca46b5e48bba318cf1ce7d28
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-04-16 13:00:22 +0200

    add: lower-footer, job completed

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -112,8 +112,8 @@ export default {
                     nameList: "DC Power Visa",
                     url: "#"
                 },
-            ]
-        }
+            ],
+            }
     }
 }
 </script>
@@ -149,8 +149,19 @@ export default {
             </div>
             <img src="../assets/img/dc-logo-bg.png" alt="DC logo" class="dc-logo">
         </div>
+
         <div class="lower-footer">
-            <div class="sign-up-btn">Sign up now!</div>
+            <div class="container">
+                <a href=""><div class="sign-up-btn">Sign up now!</div></a>
+                <div class="social-networks">
+                    <span class="follow-us">Follow us</span>
+                    <img src="../assets/img/footer-facebook.png" alt="Facebook">
+                    <img src="../assets/img/footer-twitter.png" alt="Twitter">
+                    <img src="../assets/img/footer-youtube.png" alt="">
+                    <img src="../assets/img/footer-pinterest.png" alt="">
+                    <img src="../assets/img/footer-periscope.png" alt="">
+                </div>
+            </div>
         </div>
     </footer>
 </template>
@@ -160,38 +171,70 @@ export default {
 @use "../style/partials/mixins" as *;
 @use "../style/partials/variables" as *;
 
-.upper-footer {
-    padding: 35px 0 45px 0;
-    background-image: url(../assets/img/footer-bg.jpg);
-    background-size: cover;
-    position: relative;
-    overflow: hidden;
-
-    .row{
-        @include flex(row, left, start);
-
-        ul{
-            color: white;
-            padding: 0 30px 15px 0;
-
-            h3{
-                text-transform: uppercase;
+footer{
+    .upper-footer {
+        padding: 35px 0 45px 0;
+        background-image: url(../assets/img/footer-bg.jpg);
+        background-size: cover;
+        position: relative;
+        overflow: hidden;
+    
+        .row{
+            @include flex(row, left, start);
+    
+            ul{
                 color: white;
-            }
-
-            li {
-                // debug
-                color: gray;
-                // end debug
+                padding: 0 30px 15px 0;
+    
+                h3{
+                    text-transform: uppercase;
+                    color: white;
+                }
+    
+                li {
+                    // debug
+                    color: gray;
+                    // end debug
+                }
             }
         }
+    
+        .dc-logo{
+            width: 35%;
+            position: absolute;
+            top: -90px;
+            right: 200px;
+        }
     }
+    .lower-footer {
+        background-color: $lower-footer-color;
+        padding: 20px 0;
+        .container {
+            @include flex (row, space-between, center);
+            .sign-up-btn {
+                text-transform: uppercase;
+                font-weight: 500;
+                color: white;
+                display: inline-block;
+                padding: 10px 15px;
+                border: 1px solid $banners-color;
+            }
+            .follow-us {
+                text-transform: uppercase;
+                font-weight: 600;
+                color: $banners-color;
+                display: inline-block;
+                padding-right: 10px;
+            }
+            .social-networks {
+                @include flex (row, left, center);
 
-    .dc-logo{
-        width: 35%;
-        position: absolute;
-        top: -90px;
-        right: 200px;
+                img {
+                    width: 12%;
+                    padding: 0 5px;
+                }
+            }
+        }
     }
 }
 
